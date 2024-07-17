3d882f5252d30180ccf92c34b39458c6e8deef8d
renad <renad@pursuit.org>
2024-07-17 10:40:35 -0400

    Fixed something that was caused by github problems earlier

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE songs (
   artist TEXT NOT NULL,
   album TEXT,
   time TEXT,
-  is_favorite BOOLEAN,
+  is_favorite BOOLEAN
 );
 
 
--- db/seed.sql
+++ db/seed.sql
@@ -4,11 +4,11 @@
 -- Insert seed data into the songs table, excluding the id column
 
 
-INSERT INTO songs (id, name, artist, album, time, is_favorite, playlist_id) VALUES
-(1, 'In the Hall of the Mountain King', 'Edvard Grieg', 'Peer Gynt', '2:55', true, 1),
-(2, 'Danse Macabre', 'Camille Saint-Saëns', 'Danse Macabre', '7:10', true, 2),
-(3, 'Toccata and Fugue in D Minor', 'Johann Sebastian Bach', 'Toccata and Fugue', '8:45', true, 2),
-(4, 'Night on Bald Mountain', 'Modest Mussorgsky', 'Night on Bald Mountain', '10:30', true, 3),
-(5, 'Dies Irae', 'Giuseppe Verdi', 'Requiem', '1:55', true, 2),
-(6, 'Ride of the Valkyries', 'Richard Wagner', 'Die Walküre', '5:20', true, 1),
-(7, 'Requiem', 'Wolfgang Amadeus Mozart', 'Requiem', '8:55', true, 3);
+INSERT INTO songs (id, name, artist, album, time, is_favorite) VALUES
+(1, 'In the Hall of the Mountain King', 'Edvard Grieg', 'Peer Gynt', '2:55', true),
+(2, 'Danse Macabre', 'Camille Saint-Saëns', 'Danse Macabre', '7:10', true),
+(3, 'Toccata and Fugue in D Minor', 'Johann Sebastian Bach', 'Toccata and Fugue', '8:45', true),
+(4, 'Night on Bald Mountain', 'Modest Mussorgsky', 'Night on Bald Mountain', '10:30', true),
+(5, 'Dies Irae', 'Giuseppe Verdi', 'Requiem', '1:55', true),
+(6, 'Ride of the Valkyries', 'Richard Wagner', 'Die Walküre', '5:20', true),
+(7, 'Requiem', 'Wolfgang Amadeus Mozart', 'Requiem', '8:55', true);
